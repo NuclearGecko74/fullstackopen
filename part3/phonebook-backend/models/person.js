c1ba5164548cbb3d35f5be481a6ddf141f625c30
Anton Olguin <antonolguin1@gmail.com>
2026-05-20 19:38:32 -0600

    lint

--- part3/phonebook-backend/models/person.js
+++ part3/phonebook-backend/models/person.js
@@ -3,8 +3,21 @@ const mongoose = require('mongoose')
 mongoose.connect(process.env.MONGODB_URI)
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    minLength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function(v) {
+        return /^\d{2,3}-\d+$/.test(v) && v.length >= 8
+      },
+      message: props => `${props.value} is not a valid phone number`
+    }
+  }
 })
 
 personSchema.set('toJSON', {
